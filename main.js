9d8e18bd0bc705e11d71f26e728cf0fc2e0673aa
level one <dumale.jeruzael.bscs2019@gmail.com>
2021-12-05 11:55:25 +0800

    ...

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import {initializeApp} from 'firebase/app';
 import {signupMe} from "/js/signup.js";
 
 //****-----------------------   v  DECLARE DOM OBJECTS HERE v  -----------------------****
+//You can just change the class name inside the reference
 let signup = new signupMe("346840654106540");
 let email = document.querySelector('.email');
 let fn = document.querySelector('.firstname');
@@ -17,8 +18,10 @@ let yrSec = document.querySelector('.yrSec');
 
 
 //signup.sample();
+//This is the signup button
 signupBtn.addEventListener('click', ()=>{
+  //signupNewUser Contains email, password, retype password, firstname, lastname, year&Sec, and Student ID parameter.
   signup.signupNewUser(email, pass, rePass, fn, ln, yrSec, studId);
-  
+
 });
 //signup.signupNewUser(email, pass, fn, '', ln, yrSec, studId);
